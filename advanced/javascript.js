aba7f623526606691cfc70879ce5d00391b28a2b
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-06 13:42:18 +0200

    intermediate

--- advanced/javascript.js
+++ advanced/javascript.js
@@ -5,7 +5,7 @@ for(var i=0;i<jsonData.length;i++)
     $(".container").append(`<div id="${i}" class="img-container"><img id="img${i}"src='${jsonData[i].image}'></div>`);
     $(`#${i}`).append(`<br><span>Relation: ${jsonData[i].relation}</span>`);
     $(`#${i}`).append(`<br><span>Name: ${jsonData[i].name}</span>`);
-    $(`#${i}`).append(`<p class="hidden"> hobby: ${jsonData[i].hobby}</p>`);
+    $(`#${i}`).append(`<p class="hidden">Hobby: ${jsonData[i].hobby}</p>`);
 }
 
 for(var i=0;i<jsonData.length;i++)
@@ -31,10 +31,14 @@ for(var i=0;i<jsonData.length;i++)
 
 $(document).ready(function() {
    
-    $(".imgcontainer").click(hide);
+    $(".img-container").click(show);
+    function show() {
+    $(this).find(".hidden").show();
+    }
+    $(".img-container").dblclick(hide);
     function hide() {
-      $(this).show(".hidden")
-    };
-    });
+    $(this).find(".hidden").hide();
+    }
 
-// `<img src="${jsonData[0].image}>"`
+    
+    });
